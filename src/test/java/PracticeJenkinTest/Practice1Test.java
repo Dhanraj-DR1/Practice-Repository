a3e7e90e1c9b27ef9e420d01b3a3aa7721f6b4c7
Dhanraj-DR1 <106527643+Dhanraj-DR1@users.noreply.github.com>
2022-06-01 09:28:04 +0530

    Update Practice1Test.java

--- src/test/java/PracticeJenkinTest/Practice1Test.java
+++ src/test/java/PracticeJenkinTest/Practice1Test.java
@@ -43,7 +43,7 @@ public class Practice1Test {
 			 driver=new ChromeDriver();
 			 break;
 	      }*/
-		 //changes 2
+		 //changes from branch1
 		driver.get("http://localhost:8888");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
